cc08b63ff215cc91cb1d48cb61d3465b619544d2
Bryan Tang <tangtang1228@ucla.edu>
2021-03-01 17:48:22 +0800

    debug

--- lab3b/lab3b.py
+++ lab3b/lab3b.py
@@ -9,17 +9,102 @@
 # superblock |GroupDescriptors |BlockBitmap |InodeBitmap |InodeTable |DataBlock
 import sys
 
-def read_csv(file):
+isConsistent = True
 
-    isConsistent = True
+def directory_consistency_audits(inode_dir_info, inode_par_array, inode_link_counts, inode_ref_counts, ifree):
+    global isConsistent
+
+    for child_inode in inode_dir_info:
+        # not sure
+        dir_name = inode_dir_info[child_inode][0]
+        parent_inode = inode_dir_info[child_inode][1]
+        if (child_inode in ifree) and (child_inode in inode_par_array):
+            print('DIRECTORY INODE ' + str(parent_inode) + ' NAME ' + str(dir_name) + ' UNALLOCATED INODE ' + str(child_inode))
+            isConsistent = False
+
+    for child_inode in inode_par_array:
+        dir_name = inode_dir_info[child_inode][0]
+        parent_inode = inode_par_array[child_inode]
+        # not sure
+        if (dir_name == "'..'") and child_inode != parent_inode:
+            print('DIRECTORY INODE ' + str(parent_inode) + ' NAME ' + str(dir_name) + ' LINK TO INODE ' + str(child_inode) + ' SHOULD BE ' + str(parent_inode))
+            isConsistent = False
+
+    for child_inode in inode_link_counts:
+
+        link_counts = inode_link_counts[child_inode]
+
+        actual_link = 0
+        if child_inode in inode_ref_counts:
+            actual_link = inode_ref_counts[child_inode]
+        
+        if actual_link != link_counts:
+            print('INODE ' + str(child_inode) + ' HAS ' + str(actual_link) + ' LINKS BUT LINKCOUNT IS ' + str(link_counts))
+            isConsistent = False
+
+def inode_allocation_audits(inode_link_counts, inodes_count, ifree, first_inode):
+    global isConsistent
+
+    # check for allocation of inodes
+    for k in range(inodes_count):
+        index = k + 1
+        if (index in ifree) and (index in inode_link_counts):
+            print('ALLOCATED INODE ' + str(index) + ' ON FREELIST')
+            isConsistent = False
+        elif (index not in ifree) and (index not in inode_link_counts) and (index >= first_inode or index == 2):
+            print('UNALLOCATED INODE ' + str(index) + ' NOT ON FREELIST')
+            isConsistent = False
+
+def block_consistency_audits(blockDict, blocks_count, bfree, non_reserved_block_start):
+    global isConsistent
+    # check for allocation, duplication, and unreference of blocks
+     # check allocation and reference
+    for k in range(blocks_count-1):
+        index = k + 1
+        if (index in blockDict) and (index in bfree):
+            print('ALLOCATED BLOCK ' + str(index) + ' ON FREELIST')
+            isConsistent = False
+        elif (index not in blockDict) and (index not in bfree) and (index >= non_reserved_block_start):
+            print('UNREFERENCED BLOCK ' + str(index))
+            isConsistent = False
+
+    # check duplication
+    for blk in blockDict:
+        if len(blockDict[blk]) > 1:
+            isConsistent = False
+            for piece in blockDict[blk]:
+                inode_num = int(piece[0])
+                level = int(piece[1])
+                if level == 0:
+                    blockType = 'BLOCK'
+                    offset = 0
+                if level == 1:
+                    blockType = 'INDIRECT BLOCK'
+                    offset = 12
+                elif level == 2:
+                    blockType = 'DOUBLE INDIRECT BLOCK'
+                    offset = 12 + 256
+                elif level == 3:
+                    blockType = 'TRIPLE INDIRECT BLOCK'
+                    offset = 12 + 256 + 256**2
+                
+                print('DUPLICATE ' + blockType + ' ' + str(blk) + ' IN INODE ' + str(inode_num) + ' AT OFFSET ' + str(offset))
+
+def main():
+    global isConsistent
+
+    if len(sys.argv) != 2:
+        sys.stderr.write("Invalid Argument(s)\n")
+        exit(1)
+
+    try:
+        file = open(sys.argv[1], "r")
+    except:
+        sys.stderr.write("cannot open the file\n")
+        exit(1)
+
     bfree = set()
     ifree = set()
-
-    inode_link_counts = {} # key: inode_num; value: link counts (theoretical)
-    inode_dir_info = {} # key: inode_num; value: [dir_name, parent_inode_num]
-    inode_ref_array = {} # key: inode_num; value: reference counts (actual)
-    inode_par_array = {} # key: child_inode_num; value: parent_inode_num
-    blockDict = {} # key: block number; value: [inode_num, level]
 
     numOfInodes = 0
     blocks_count = 0
@@ -29,6 +114,12 @@ def read_csv(file):
 
     non_reserved_block_start = 0
     first_inode = 0
+
+    inode_dir_info = {} # key: inode_num; value: [dir_name, parent_inode_num]
+    inode_link_counts = {} # key: inode_num; value: link counts (theoretical)
+    inode_ref_counts = {} # key: inode_num; value: reference counts (actual)
+    inode_par_array = {} # key: child_inode_num; value: parent_inode_num
+    blockDict = {} # key: block number; value: [inode_num, level]
 
     lines = file.readlines()
 
@@ -45,7 +136,6 @@ def read_csv(file):
         elif Type == 'GROUP':
             numOfInodes = int(fields[3])
             bg_inode_table = int(fields[8])
-
             non_reserved_block_start = bg_inode_table + inode_size * numOfInodes / block_size
 
         elif Type == 'BFREE':
@@ -129,10 +219,10 @@ def read_csv(file):
             # slice off the '\n' character
             dir_name = dir_name[:-1]
 
-            if inode_num in inode_ref_array:
-                inode_ref_array[inode_num] = inode_ref_array[inode_num] + 1
+            if inode_num in inode_ref_counts:
+                inode_ref_counts[inode_num] = inode_ref_counts[inode_num] + 1
             else:
-                inode_ref_array[inode_num] = 1
+                inode_ref_counts[inode_num] = 1
 
             inode_dir_info[inode_num] = [str(dir_name), int(parent_inode_num)]
         
@@ -148,93 +238,12 @@ def read_csv(file):
             if str(dir_name) != "'.'" and str(dir_name) != "'..'":
                 inode_par_array[inode_num] = parent_inode_num
         
-    # check for allocation, duplication, and unreference of blocks
-     # check allocation and reference
-    for k in range(blocks_count-1):
-        index = k + 1
-        if (index in blockDict) and (index in bfree):
-            print('ALLOCATED BLOCK ' + str(index) + ' ON FREELIST')
-            isConsistent = False
-        elif (index not in blockDict) and (index not in bfree) and (index >= non_reserved_block_start):
-            print('UNREFERENCED BLOCK ' + str(index))
-            isConsistent = False
 
-    # check duplication
-    for blk in blockDict:
-        if len(blockDict[blk]) > 1:
-            isConsistent = False
-            for piece in blockDict[blk]:
-                inode_num = int(piece[0])
-                level = int(piece[1])
-                if level == 0:
-                    blockType = 'BLOCK'
-                    offset = 0
-                if level == 1:
-                    blockType = 'INDIRECT BLOCK'
-                    offset = 12
-                elif level == 2:
-                    blockType = 'DOUBLE INDIRECT BLOCK'
-                    offset = 12 + 256
-                elif level == 3:
-                    blockType = 'TRIPLE INDIRECT BLOCK'
-                    offset = 12 + 256 + 256**2
-                
-                print('DUPLICATE ' + blockType + ' ' + str(blk) + ' IN INODE ' + str(inode_num) + ' AT OFFSET ' + str(offset))
+    directory_consistency_audits(inode_dir_info, inode_par_array, inode_link_counts, inode_ref_counts, ifree)
 
-    # check for allocation of inodes
-    for k in range(inodes_count):
-        index = k + 1
-        if (index in ifree) and (index in inode_link_counts):
-            print('ALLOCATED INODE ' + str(index) + ' ON FREELIST')
-            isConsistent = False
-        elif (index not in ifree) and (index not in inode_link_counts) and (index >= first_inode or index == 2):
-            print('UNALLOCATED INODE ' + str(index) + ' NOT ON FREELIST')
-            isConsistent = False
+    inode_allocation_audits(inode_link_counts, inodes_count, ifree, first_inode)
 
-    for child_inode in inode_dir_info:
-        # not sure
-        dir_name = inode_dir_info[child_inode][0]
-        parent_inode = inode_dir_info[child_inode][1]
-        if (child_inode in ifree) and (child_inode in inode_par_array):
-            print('DIRECTORY INODE ' + str(parent_inode) + ' NAME ' + str(dir_name) + ' UNALLOCATED INODE ' + str(child_inode))
-            isConsistent = False
-
-    for child_inode in inode_par_array:
-        dir_name = inode_dir_info[child_inode][0]
-        parent_inode = inode_par_array[child_inode]
-        # not sure
-        if (dir_name == "'..'") and child_inode != parent_inode:
-            print('DIRECTORY INODE ' + str(parent_inode) + ' NAME ' + str(dir_name) + ' LINK TO INODE ' + str(child_inode) + ' SHOULD BE ' + str(parent_inode))
-            isConsistent = False
-
-    for child_inode in inode_link_counts:
-
-        link_counts = inode_link_counts[child_inode]
-
-        actual_link = 0
-        if child_inode in inode_ref_array:
-            actual_link = inode_ref_array[child_inode]
-        
-        if actual_link != link_counts:
-            print('INODE ' + str(child_inode) + ' HAS ' + str(actual_link) + ' LINKS BUT LINKCOUNT IS ' + str(link_counts))
-            isConsistent = False
-        
-    return isConsistent
-
-
-
-def main():
-    if len(sys.argv) != 2:
-        sys.stderr.write("Invalid Argument(s)\n")
-        exit(1)
-
-    try:
-        input_file = open(sys.argv[1], "r")
-    except:
-        sys.stderr.write("cannot open the file\n")
-        exit(1)
-
-    isConsistent = read_csv(input_file)
+    block_consistency_audits(blockDict, blocks_count, bfree, non_reserved_block_start)
 
     if isConsistent:
         exit(0)
